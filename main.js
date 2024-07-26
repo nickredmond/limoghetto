fec82de35cd9a2428b5d6dfeeb6414591aa63a1f
Nick Redmond <nicholas.john.redmond@gmail.com>
2024-07-26 15:32:33 -0700

    starry background

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { MeshSurfaceSampler } from 'three/addons/math/MeshSurfaceSampler.js';
 import { handleTouchMove, handleTouchEnd } from './touches.js'
 import { initFloors, updateFloors } from './floors.js'
 
@@ -33,6 +34,44 @@ renderer.domElement.addEventListener('touchend', (evt) => {
 })
 
 initFloors(scene)
+
+//
+const geometry = new THREE.SphereGeometry(120);
+const material = new THREE.MeshBasicMaterial({
+  color: 0x000000,
+  wireframe: true
+});
+const cube = new THREE.Mesh(geometry, material);
+cube.position.y = 5
+scene.add(cube);
+
+//const sampler = new THREE.MeshSurfaceSampler(cube).build()
+const sampler = new MeshSurfaceSampler( cube )
+	.setWeightAttribute( 'color' )
+	.build();
+
+const vertices = [];
+const tempPosition = new THREE.Vector3();
+for (let i = 0; i < 8000; i ++) {
+  sampler.sample(tempPosition);
+  vertices.push(tempPosition.x, tempPosition.y, tempPosition.z);
+}
+
+/* Create a geometry from the coordinates */
+const pointsGeometry = new THREE.BufferGeometry();
+pointsGeometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+
+/* Create a material */
+const pointsMaterial = new THREE.PointsMaterial({
+  color: 0xffffff,
+  size: Math.random() * 0.5 + 0.1
+});
+/* Create a Points object */
+const points = new THREE.Points(pointsGeometry, pointsMaterial);
+points.position.y = 5
+/* Add the points into the scene */
+scene.add(points);	
+//
 
 camera.position.y = 5
 camera.position.z = 5
