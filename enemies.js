f73f73a8245c48bbec7da7acf7a43d31776a1d58
Nick Redmond <nicholas.john.redmond@gmail.com>
2024-07-28 13:22:10 -0700

    enemies move toward player

--- enemies.js
+++ enemies.js
@@ -1,8 +1,10 @@
 import {
   SphereGeometry,
-  MeshBasicMaterial,
-  Mesh
+  MeshStandardMaterial,
+  Mesh,
+  Vector3
 } from 'three'
+import { removeObject3D } from './utils.js'
 
 let scene;
 let isEnemyTimeout = false
@@ -12,7 +14,7 @@ export function initEnemies(scn) {
 }
 
 function addEnemy() {
-  const dist = 15
+  const dist = 100
   const x1 = 0
   const y1 = 5
   const z1 = 5
@@ -24,22 +26,41 @@ function addEnemy() {
     ((y2 - y1) * (y2 - y1))
   ) + z1
   
-  const geometry = new SphereGeometry(1);
-  const material = new MeshBasicMaterial({
+  const geometry = new SphereGeometry(2);
+  const material = new MeshStandardMaterial({
     color: 0xff0000
   });
+  
+  const enemyLoc = new Vector3(x2, y2, z2)
+  const playerLoc = new Vector3(x1, y1, z1)
+  const direction = new Vector3()
+  direction.subVectors(playerLoc, enemyLoc).normalize()
+  
   const sphere = new Mesh(geometry, material);
-  sphere.position.x = x2
+  sphere.position.x = x2 
   sphere.position.y = y2
   sphere.position.z = -z2
   scene.add(sphere);
+  
+  direction.x *= 0.5
+  direction.y *= 0.5
+  direction.z *= 0.5
+  const interval = setInterval(() => {
+   sphere.position.x += direction.x
+   sphere.position.y += direction.y
+   sphere.position.z -= direction.z
+   if (sphere.position.z >= 5) {
+     removeObject3D(sphere)
+     clearInterval(interval)
+   }
+  }, 1000 / 60)
 }
 
 export function updateEnemies() {
   if (!isEnemyTimeout) {
     isEnemyTimeout = true
     addEnemy()
-    const enemyTimeout = Math.random() * 1000 + 200
+    const enemyTimeout = Math.random() * 2000 + 500
     setTimeout(function() {
       isEnemyTimeout = false
     }, enemyTimeout)
